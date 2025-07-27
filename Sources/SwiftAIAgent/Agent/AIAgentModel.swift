6d0521a23155f1e05f52ac9bdfd02ef9da3ead8f
tim wang <overocean@gail.com>
2025-07-27 15:10:56 +1000

    Add Swift Package Index DocC config

--- Sources/SwiftAIAgent/Agent/AIAgentModel.swift
+++ Sources/SwiftAIAgent/Agent/AIAgentModel.swift
@@ -4,16 +4,16 @@ import AIAgentMacros
 public protocol AIAgentModel: Sendable {
 
     /// Rum prompt with LLM with structured output scheme
-    /// Parameters:
-    /// - prompt: the promppt to be sent to LLM
-    /// - outputSchema: the json scheme of the expected output
-    /// Returns: A wrapper of all types of output of LLM
+    /// - Parameters:
+    ///  - prompt: the promppt to be sent to LLM
+    ///  - outputSchema: the json scheme of the expected output
+    /// - Returns: A wrapper of all types of output of LLM
     func run(prompt: String, outputSchema: String?) async throws -> AIAgentOutput
 
     /// Rum prompt with LLM with structured output scheme
-    /// Parameters:
-    /// - prompt: the promppt to be sent to LLM
-    /// - outputSchema: the json scheme of the expected output using Swift Strong type
-    /// Returns: A wrapper of all types of output of LLM
+    /// - Parameters:
+    ///  - prompt: the promppt to be sent to LLM
+    ///  - outputSchema: the json scheme of the expected output using Swift Strong type
+    /// - Returns: A wrapper of all types of output of LLM
     func run<T: AIModelOutput>(prompt: String, outputSchema: T.Type) async throws -> AIAgentOutput
 }
