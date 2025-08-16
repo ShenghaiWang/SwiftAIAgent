071891eb7571da0b3dbb248c73b4d6c367920525
tim wang <overocean@gail.com>
2025-08-17 00:19:43 +1000

    Refine logic for clarification

--- Sources/SwiftAIAgent/Agents/GoalManager.swift
+++ Sources/SwiftAIAgent/Agents/GoalManager.swift
@@ -61,12 +61,14 @@ public actor GoalManager {
     /// - Returns: The solution output from the agent for the goal
     /// - Throws: `GoalManager.Error.needClarification(questions: [String])` in case of needing clarification for the goal.
     /// - Throws: `Swift.Error` in case of any network error or LLM error.
-    public func run() async throws -> [AIAgentOutput] {
-        logger.debug("\n===Checking Goal===\n")
-        let clarification: AIGoalClarification = try await runAICommand(clarifyInstructions)
-        logger.debug("\n===Clarification Questions===\n\(clarification)\n")
-        if !clarification.questions.isEmpty {
-            throw Error.needClarification(questions: clarification.questions)
+    public func run(noFutherClarification: Bool = false) async throws -> [AIAgentOutput] {
+        if !noFutherClarification {
+            logger.debug("\n===Checking Goal===\n")
+            let clarifications = try await clarify()
+            logger.debug("\n===Clarification Questions===\n\(clarifications)\n")
+            if !clarifications.isEmpty {
+                throw Error.needClarification(questions: clarifications)
+            }
         }
         logger.debug("\n===Starting Planning===\n")
         let task: AITask = try await runAICommand(planInstructions)
@@ -74,6 +76,16 @@ public actor GoalManager {
         let workflow = try await workflow(for: task)
         logger.debug("\n===Workflow Planned===\n\(workflow)\n")
         return try await workflow.run(prompt: "kick off the task")
+    }
+
+    /// Clarify the goal
+    /// - Returns: The calriciation questions for the goal
+    /// - Throws: `Swift.Error` in case of any network error or LLM error.
+    public func clarify() async throws -> [String] {
+        logger.debug("\n===Checking Goal===\n")
+        let clarification: AIGoalClarification = try await runAICommand(clarifyInstructions)
+        logger.debug("\n===Clarification Questions===\n\(clarification)\n")
+        return clarification.questions
     }
 
     private func runAICommand<T: AIModelSchema>(_ command: String) async throws -> T {
